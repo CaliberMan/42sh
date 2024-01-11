e8c4edbf4d7cd39d0be4d111722fe7c99f68fdae
Térence Miralves <terence.miralves@gmail.com>
2024-01-11 23:25:37 +0100

    feat(test): Added tests for pretty print can be used as a test template for later

---
+++ tests/test_pretty_print.c
@@ -0,0 +1,43 @@
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "../src/pretty_print/pretty_print.h"
+#include <string.h>
+
+void redirect_all_stdout(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+int error(void)
+{
+    write(2, "error", 5);
+    return (0);
+}
+
+Test(Pretty_Print_EasyTest, Empty, .init = redirect_all_stdout)
+{
+    cr_expect_eq(pretty_print(NULL), 0, "Should return 0 and print nothing");
+    fflush(NULL);
+    cr_expect_stdout_eq_str("", "Printed something when shouldn't have");
+}
+Test(Pretty_Print_EasyTest, SecondTest, .init = redirect_all_stdout)
+{
+    struct ast ast;
+    ast.type = AST_COMMAND;
+    ast.command = malloc(sizeof(char *) * 3);
+    ast.command[0] = malloc(10);
+    ast.command[1] = malloc(10);
+    ast.command[2] = malloc(5);
+    strcpy(ast.command[0], "echo");
+    strcpy(ast.command[1], "Hello World");
+    ast.command[2] = NULL;
+    cr_expect_eq(pretty_print(&ast), 0, "Should return 0");
+    fflush(NULL);
+    cr_expect_stdout_eq_str("command \"echo\" \"Hello World\"", "Wrong format output for echo hello world");
+    free(ast.command[0]);
+    free(ast.command[1]);
+    free(ast.command[2]);
+}
