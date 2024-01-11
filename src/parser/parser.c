9efb48068ec431dd283b0cc70836a4f10e975450
Raj Sunil Mahajan Aka Mali <raj-sunil-mahajan-aka-mali@epita.fr>
2024-01-11 13:18:30 +0100

    feat(parser.c): wrote all rules but if

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -61,3 +61,49 @@ enum parser_status parse_command(struct ast **ast, struct lexer *lexer)
 
     return parse_shell_command(ast, lexer);
 }
+
+enum parser_status parse_simple_command(struct ast **ast, struct lexer *lexer)
+{
+    struct token *token = lexer_peek(lexer);
+    if (token->type != TOKEN_WORD)
+        return PARSER_ERROR;
+
+    struct ast *command_node = init_ast(AST_COMMAND);
+    command_node->command[0] = token->data;
+
+    command->left = *ast;
+
+    lexer_pop(lexer);
+    int index = 1;
+
+    while (1)
+    {
+        struct ast *next_word;
+        enum parser_status status = parse_element(&next_word, lexer);
+        if (status == PARSER_ERROR)
+            return PARSER_OK;
+
+        command_node->command[index++] = next_word->command[0];
+    }
+
+    *ast = command_node;
+    return PARSER_OK;
+}
+
+enum parser_status parse_element(struct ast **ast, struct lexer *lexer)
+{
+    struct token *token = lexer_peek(lexer);
+    if (token->type != TOKEN_WORD)
+        return PARSER_ERROR;
+
+    struct ast *node = init_ast(AST_COMMAND);
+    node->command[0] = token->data;
+    *ast = node;
+
+    return PARSER_OK;
+}
+
+enum parser_status parse_shell_command(struct ast **ast, struct lexer *lexer)
+{
+
+}
