bb10a5d767aa99aa7d592865d4ebc3e601c72aab
David Calderon <david-ortiz.calderon@epita.fr>
2024-01-12 21:28:55 +0100

    feat(exec): added exec folder

--- tests/test_pretty_print.c
+++ tests/test_pretty_print.c
@@ -1,4 +1,5 @@
 #include <criterion/criterion.h>
+#include <stdlib.h>
 #include <criterion/redirect.h>
 #include <stdio.h>
 #include <string.h>
@@ -30,14 +31,14 @@ Test(Pretty_Print_EasyTest, SecondTest, .init = redirect_all_stdout)
     ast.type = AST_COMMAND;
     ast.command = malloc(sizeof(char *) * 3);
     ast.command[0] = malloc(10);
-    ast.command[1] = malloc(10);
+    ast.command[1] = malloc(21);
     ast.command[2] = malloc(5);
     strcpy(ast.command[0], "echo");
     strcpy(ast.command[1], "Hello World");
     ast.command[2] = NULL;
     cr_expect_eq(pretty_print(&ast), 0, "Should return 0");
     fflush(NULL);
-    cr_expect_stdout_eq_str("command \"echo\" \"Hello World\"",
+    cr_expect_stdout_eq_str("command \"echo\" \"Hello World\" ",
                             "Wrong format output for echo hello world");
     free(ast.command[0]);
     free(ast.command[1]);
