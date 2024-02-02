6f3ffb14a91eab15690cc61ecdd71681b5b4ae0e
Raj Sunil Mahajan Aka Mali <raj-sunil.mahajan-aka-mali@epita.fr>
2024-02-02 04:53:46 +0000

    feat: wrote case ast

---
+++ src/ast/case/ast_case.c
@@ -0,0 +1,21 @@
+#include "ast_case.h"
+
+#include "../ast.h"
+
+void free_case(struct ast_case *ast)
+{
+    free_ast(ast->expr);
+    ast->expr = NULL;
+
+    free_ast(ast->cases_list);
+    ast->cases_list = NULL;
+}
+
+void free_pattern(struct ast_pattern *ast)
+{
+    free_ast(ast->pattern);
+    ast->pattern = NULL;
+
+    free_ast(ast->statement);
+    ast->statement = NULL;
+}
---
+++ src/ast/case/ast_case.h
@@ -0,0 +1,34 @@
+#ifndef AST_CASE_H
+#define AST_CASE_H
+
+struct ast_case
+{
+    struct ast *expr;
+    struct ast *cases_list; // ast_list of ast_case
+};
+
+struct ast_pattern
+{
+    struct ast *pattern;
+    struct ast *statement;
+};
+
+/**
+ * case EXPR in 
+ *  PATTERN_1)
+ *      STATEMENT
+ *  PATTERN_2)
+ *      STATEMENT =>                ast_case -> expr
+ *  *)                                 ↓
+ *      STATEMENT                   cases_list ...
+ *  esac                            ↙         ↘
+ *                        ast_pattern         ast_pattern
+ *                          ↙       ↘           ↙      ↘
+ *                      pattern_1 statement pattern_2  statement
+ */
+
+void free_pattern(struct ast_pattern *ast);
+
+void free_case(struct ast_case *ast);
+
+#endif /* ! AST_CASE_H */
