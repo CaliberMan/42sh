8bf32b0edcd78b19c66cfa2022a83951b0f70dc1
Raj Sunil Mahajan Aka Mali <raj-sunil-mahajan-aka-mali@epita.fr>
2024-02-03 16:24:28 +0100

    fix: write memory access on for lexer test7

--- src/parser/shell_cmd/loops/parser_for.c
+++ src/parser/shell_cmd/loops/parser_for.c
@@ -107,6 +107,10 @@ static enum parser_status parse_body(struct ast **ast, struct lexer *lexer)
 enum parser_status parse_for(struct ast **ast, struct lexer *lexer)
 {
     lexer_pop(lexer);
+    struct ast *for_loop = init_ast(AST_LOOP);
+    for_loop->data.ast_loop.type = FOR_LOOP;
+    *ast = for_loop;
+
     struct token *token = lexer_peek(lexer);
     if (token->type != TOKEN_WORD)
     {
@@ -116,13 +120,8 @@ enum parser_status parse_for(struct ast **ast, struct lexer *lexer)
 
     lexer_pop(lexer);
 
-    struct ast *for_loop = init_ast(AST_LOOP);
-    for_loop->data.ast_loop.type = FOR_LOOP;
-
     // got variable name
     copy_word(token, for_loop, -1);
-    *ast = for_loop;
-
     token_free(token);
 
     // check rule1
